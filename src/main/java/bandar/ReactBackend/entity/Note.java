d56bc433dfbafc50894697eaec50cda41dfeb311
bandarProgramer <bandar.programer@gamil.com>
2021-03-31 13:14:07 +0300

    created rest end point for new note

--- src/main/java/bandar/ReactBackend/entity/Note.java
+++ src/main/java/bandar/ReactBackend/entity/Note.java
@@ -1,10 +1,16 @@
 package bandar.ReactBackend.entity;
 
+import java.util.Date;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import lombok.Data;
 
@@ -22,4 +28,12 @@ public class Note {
 	private String body;
 	
 	private String category;
+	
+	@Column(name = "created_at", nullable = false, updatable = false)
+	@CreationTimestamp
+	private Date createdAt;
+	
+	@Column(name = "updated_at", nullable = false, updatable = false)
+	@UpdateTimestamp
+	private Date updatedAt;
 }
